13d0f7a42d6cb83580fef4248dddb0fa1cd499df
Narasimman <narasi.mit@gmail.com>
2016-04-02 20:02:04 -0400

    parse inputs

--- src/main/java/prog3/PageRanker.java
+++ src/main/java/prog3/PageRanker.java
@@ -1,11 +1,22 @@
 package prog3;
 
+import java.io.File;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.util.List;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 public class PageRanker {
 
   private final Path collectionPath;
+  private final File[] files;
+  private List<Page> pages;
+  
   private final int numberOfPages;
   
   private static final double epsilonFactor = 0.01;
@@ -16,16 +27,32 @@ public class PageRanker {
   
   public PageRanker(String filePath, double F) {
     
-    collectionPath = FileSystems.getDefault().getPath(filePath);    
-    numberOfPages = 0;
-    
+    collectionPath = FileSystems.getDefault().getPath(filePath);
+    files = collectionPath.toFile().listFiles();
+    numberOfPages = files.length;
     epsilon = epsilonFactor/numberOfPages;
     this.F = F;
-    
     weight = new double[numberOfPages][numberOfPages];
   }
   
   public static void main(String[] args) {
+    CommandLine cmd = null;
+    // set options
+    Options options = new Options();
+    options.addOption("docs", "docs", true, "collection of document");
+    options.addOption("f", "f", true, "F");
+    
+    CommandLineParser parser = new DefaultParser();
+    try {
+      cmd = parser.parse(options, args);
+    } catch (ParseException e) {
+      e.printStackTrace();
+    }
+    
+    String path = cmd.getOptionValue("docs");
+    double F = Double.parseDouble(cmd.getOptionValue("f"));
+    
+    PageRanker pr = new PageRanker(path, F);    
     
   }
 
